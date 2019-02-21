e7f70faf306dbb496e1006575823815476bf9c24
devendra.singh_ca18@gla.ac.in <devendra.singh_mca18@gla.ac.in>
2019-02-21 11:20:24 +0530

    add a action and a msg for snackbar

--- app/src/main/java/singh/devendra/gla2/MainActivity.java
+++ app/src/main/java/singh/devendra/gla2/MainActivity.java
@@ -22,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Devendra Singh", Snackbar.LENGTH_LONG)
+                        .setAction("OK", null).show();
             }
         });
     }
